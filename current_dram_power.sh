f41cf190ba86b176e7e435a681a53600d318d5d4
Dane <danesherbs@gmail.com>
2020-02-14 23:29:34 +1100

    Change DRAM power from micro W to W

--- current_dram_power.sh
+++ current_dram_power.sh
@@ -5,6 +5,6 @@ e1=$(cat $dram/energy_uj)
 SECONDS=0
 sleep 1
 e2=$(cat $dram/energy_uj)
-power=$(bc <<< "scale=2; ($e2-$e1)/$SECONDS")
+power=$(bc <<< "scale=2; ($e2-$e1)/$SECONDS/1000000")
 
 echo $power
