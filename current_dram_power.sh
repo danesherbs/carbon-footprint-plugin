2076f2224389d937281afaf2001af1659c2f2f1c
Dane <danesherbs@gmail.com>
2020-02-14 23:25:39 +1100

    Add DRAM power consumption script

---
+++ current_dram_power.sh
@@ -0,0 +1,10 @@
+pkg=/sys/class/powercap/intel-rapl/intel-rapl:0
+dram=$pkg/intel-rapl:0:0
+
+e1=$(cat $dram/energy_uj)
+SECONDS=0
+sleep 1
+e2=$(cat $dram/energy_uj)
+power=$(bc <<< "scale=2; ($e2-$e1)/$SECONDS")
+
+echo $power
